43160fb06fa58fed0f8dc4ba41b01e9166145671
will chapman <williamchapman53@gmail.com>
2025-04-14 16:42:11 -0400

    added expense model

---
+++ src/main/java/com/SamuelBusses/ExpensesTracker/Models/Expenses.java
@@ -0,0 +1,69 @@
+package com.SamuelBusses.ExpensesTracker.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Expenses {
+
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+private Long expenseId;
+
+private String category;
+private Double cost;
+private String description;
+private String month;
+
+    public Expenses(Long expenseId, String category, Double cost, String description, String month) {
+        this.expenseId = expenseId;
+        this.category = category;
+        this.cost = cost;
+        this.description = description;
+        this.month = month;
+    }
+
+    public Expenses(){}
+
+    public Long getExpenseId() {
+        return expenseId;
+    }
+
+    public void setExpenseId(Long expenseId) {
+        this.expenseId = expenseId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+}
