a65fcd331e8d95abaa1844eee859a39516bfdee0
will chapman <williamchapman53@gmail.com>
2025-04-14 16:23:22 -0400

    first commit

---
+++ src/test/java/com/SamuelBusses/ExpensesTracker/ExpensesTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SamuelBusses.ExpensesTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
