508f85cad049aaa80cf7b294d762c5c4042c3495
will chapman <williamchapman53@gmail.com>
2025-05-26 12:41:17 -0400

    adding account feature

---
+++ src/main/java/com/SamuelBusses/ExpensesTracker/Controllers/AccountController.java
@@ -0,0 +1,7 @@
+package com.SamuelBusses.ExpensesTracker.Controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+}
---
+++ src/main/java/com/SamuelBusses/ExpensesTracker/Repositories/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.SamuelBusses.ExpensesTracker.Repositories;
+
+import com.SamuelBusses.ExpensesTracker.Models.Account;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends CrudRepository <Account, Long> {
+}
