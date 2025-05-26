d1010db7708fefe62304f6f0b6aa1db96336e0b4
will chapman <williamchapman53@gmail.com>
2025-05-26 12:55:38 -0400

    added id to account entity

--- src/main/java/com/SamuelBusses/ExpensesTracker/Models/Account.java
+++ src/main/java/com/SamuelBusses/ExpensesTracker/Models/Account.java
@@ -1,7 +1,14 @@
 package com.SamuelBusses.ExpensesTracker.Models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
 }
