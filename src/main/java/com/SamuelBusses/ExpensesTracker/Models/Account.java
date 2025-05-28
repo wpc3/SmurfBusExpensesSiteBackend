f499ccde26234a7c6f39b81bffa324c9a8b4011c
will chapman <williamchapman53@gmail.com>
2025-05-28 11:16:26 -0400

    created relationship with account and expense

--- src/main/java/com/SamuelBusses/ExpensesTracker/Models/Account.java
+++ src/main/java/com/SamuelBusses/ExpensesTracker/Models/Account.java
@@ -1,9 +1,9 @@
 package com.SamuelBusses.ExpensesTracker.Models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Account {
@@ -11,4 +11,41 @@ public class Account {
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
+
+    private String password;
+
+    @OneToMany(mappedBy = "account")
+    private Set<Expenses> expenses = new HashSet<>();
+
+
+    public Account(){}
+
+    public Account(Long id, String password) {
+        this.id = id;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Expenses> getExpenses() {
+        return expenses;
+    }
+
+    public void setExpenses(Set<Expenses> expenses) {
+        this.expenses = expenses;
+    }
 }
