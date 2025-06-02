57179e68e6cf67c6a6f5f3d2273c834b05dd49b4
will chapman <williamchapman53@gmail.com>
2025-06-02 11:41:58 -0400

    added an add expense with account logic and endpoint

--- src/main/java/com/SamuelBusses/ExpensesTracker/Service/AccountService.java
+++ src/main/java/com/SamuelBusses/ExpensesTracker/Service/AccountService.java
@@ -2,17 +2,24 @@ package com.SamuelBusses.ExpensesTracker.Service;
 
 import com.SamuelBusses.ExpensesTracker.Exceptions.UnauthorizedException;
 import com.SamuelBusses.ExpensesTracker.Models.Account;
+import com.SamuelBusses.ExpensesTracker.Models.Expenses;
 import com.SamuelBusses.ExpensesTracker.Repositories.AccountRepository;
+import com.SamuelBusses.ExpensesTracker.Repositories.ExpensesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 public class AccountService {
 
    private AccountRepository accountRepository;
+   private ExpensesRepository expensesRepository;
 
-    public AccountService(@Autowired AccountRepository accountRepository) {
+    public AccountService(@Autowired AccountRepository accountRepository,
+                          @Autowired ExpensesRepository expensesRepository) {
         this.accountRepository = accountRepository;
+        this.expensesRepository = expensesRepository;
     }
 
     public Account createAnAccount(Account account){
@@ -50,6 +57,19 @@ public class AccountService {
                 .orElseThrow(() -> new UnauthorizedException("Invalid username or password"));
 
 
+    }
+
+    public Account saveAnExpenseToAccount(Long accountId, Expenses expenses){
+
+        Account account = accountRepository.findById(accountId).orElseThrow(() -> new RuntimeException("account id does not exist"));
+//        Expenses expenses = expensesRepository.findById(expenseId).orElseThrow(() -> new RuntimeException("expense id does not exist"));
+
+       expenses.setAccount(account);
+
+       account.getExpenses().add(expenses);
+
+
+        return accountRepository.save(account);
     }
 
 
